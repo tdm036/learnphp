e7dbeac38803979ab58ec6dd513bf3e650775b47
tdm036 <tdm036@gmail.com>
2025-11-20 21:07:59 +0300

    Рассмотрели конструкцию 'if-else'

---
+++ ifelse.php
@@ -0,0 +1,26 @@
+<?php
+
+    $a = 5;
+    $b = -10;
+
+    if ($a > $b)
+    {
+        echo 'a больше b';
+    }
+    else
+        echo 'a меньше b';
+
+    echo '<br>';
+
+    $variable5 = 70;
+    if ($variable5 < 0)
+    {
+        echo 'Значение отрицательное';
+    }
+    elseif ($variable5 > 0)
+    {
+        echo 'Значение положительное';
+    }
+    else
+        echo 'Равно нулю';
+?>
